820b87bec3bcb423badc4dc961c662d43c80271e
Niks1906 <66248757+Niks1906@users.noreply.github.com>
2021-01-22 10:57:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,95 +1,60 @@
-// Add matter.js first
-// give the physics engines
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-const Render = Matter.Render;
-// create the array of ranindrops
-var raindrops = [];
-// make  a new umbrella 
-var img;
-// give image to the background
-var backgroundImg;
-// give image to thunder
-var thunder1,thunder2 ,thunderImg1 , thunderImg2 , thunderImg3;
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var engine,world;
+var umbrella;
+var maxDrops=100;
+var drops=[];
+
 function preload(){
-  // load bg image
-  backgroundImg = loadImage("Image/bg.jpg");
-  // load both thunder images
-    thunder1 = loadImage("Image/i1 (1).png");
-    thunder2 = loadImage("Image/i1 (2).png");
+    thunder1Img=loadImage("thunderbolt/1.png")
+    thunder2Img=loadImage("thunderbolt/2.png")
+    thunder3Img=loadImage("thunderbolt/3.png")
+    thunder4Img=loadImage("thunderbolt/4.png")
 }
 
 function setup(){
-  // create canvas
-var canvas = createCanvas(displayWidth - 20,displayHeight - 30);
-  // crate engine + world
-    engine = Engine.create();
-    world = engine.world;
-  // make sprites of thunderImages 1 , 2 , 3 
-  //  give positions to the sprites
-thunderImg1 = createSprite(displayWidth/2,5,10,40);
-thunderImg2 = createSprite(200,5,10,40);
-thunderImg3 = createSprite(1100,5,10,40);
-// give the positions to img of umbrella
-    img = new Umbrella(displayWidth/2,500);
+createCanvas(600,900);
+engine=Engine.create();
+world=engine.world;
+
+umbrella=new Umbrella(300,640);
+
+for(var i=0;i<maxDrops;i++){
+  drops.push(new Drops(random(1,600),random(1,900)));
+}
+
 }
 
 function draw(){
-  // add image to the background
-    background(backgroundImg);
-  // update Engine
-    Engine.update(engine);
-   // create raindrops by frame count
-    if(frameCount % 1 === 0){
-      var raindrop = new Drop(random(0, windowWidth), -100,10);
-      raindrops.push(raindrop);
-    }
-// display umbrella img
-    img.display();
-// display raindrops
-    for(var i = 0; i < raindrops.length; i++){
-      raindrops[i].fall(20);
-      raindrops[i].display();
-    } 
-// add random img to the thunder images 1
-  // By Using Switch case method
-    if(frameCount % 10 === 0) {
-    var rand = Math.round(random(1,2));
-      switch(rand) {
-        case 1:thunderImg1.addImage(thunder1);
-                break;
-        case 2: thunderImg1.addImage(thunder2);
-                break;
-        default: break;
-      }
-    }   
-  // add random img to the thunder images 2
-  // By Using Switch case method
-    if(frameCount % 10 === 0) {
-      var rand = Math.round(random(1,2));
-        switch(rand) {
-          case 1:thunderImg2.addImage(thunder1);
-                  break;
-          case 2: thunderImg2.addImage(thunder2);
-                  break;
-          default: break;
-        }
-      }   
-  // add random img to the thunder images 3
-  // By Using Switch case method
-      if(frameCount % 10 === 0) {
-        var rand = Math.round(random(1,2));
-          switch(rand) {
-            case 1:thunderImg3.addImage(thunder1);
-                    break;
-            case 2: thunderImg3.addImage(thunder2);
-                    break;
-            default: break;
-          }
-        } 
-  // for drawing the sprites write drawSprites at the end 
-    drawSprites();
-}   
+ background(0);
+ Engine.update(engine);
+ 
+ umbrella.display();
+ for(var i=0;i<maxDrops;i++){
+   drops[i].update();
+   drops[i].display();
+ }
+
+ if (frameCount%60===0){
+   console.log("hello");
+  var thunder=createSprite(random(50,550),40,40,50)
+  thunder.lifetime=20;
+  var num=Math.round(random(1,4));
+  switch(num){
+    case 1:thunder.addImage(thunder1Img)
+    break;
+    case 2:thunder.addImage(thunder2Img)
+    break;
+    case 3:thunder.addImage(thunder3Img)
+    break;
+    case 4:thunder.addImage(thunder4Img)
+    break;
+  }
+}
+
+drawSprites();   
+}
+
 
